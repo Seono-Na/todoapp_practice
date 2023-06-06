aa9b90cc6e0a469517eae25f5ebdf854b05b1527
Seono-Na <seono5670@gmail.com>
2023-06-07 04:01:49 +0900

    uuid로 id 부여

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState("");
@@ -9,7 +10,7 @@ export default function AddTodo({ onAdd }) {
       setText("");
       return;
     }
-    onAdd({ id: "original", text, status: "active" });
+    onAdd({ id: uuidv4(), text, status: "active" });
     setText("");
   };
   return (
