ee2d864778eaad4763ec4b9e60f0ba155d3ce088
Seono-Na <seono5670@gmail.com>
2023-06-06 18:57:19 +0900

    추가하기 오타 수정

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 
 export default function TodoList() {
-  const [todos, setTodo] = useState([
+  const [todos, setTodos] = useState([
     { id: "123", text: "장보기", status: "active" },
     { id: "124", text: "공부하기", status: "active" },
   ]);
   const handleAdd = (todo) => {
-    setTodo([...todos, todo]);
+    setTodos([...todos, todo]);
   };
   return (
     <section>
